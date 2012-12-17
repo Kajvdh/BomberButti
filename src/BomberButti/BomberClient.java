998fd8283816b9816124e9203a82eb970f735e94
Kajvdh <Kaj@telenet.be>
2012-12-17 20:52:07 +0100

    Built in standaard Server<-->Client communication test
    Signed-off-by: Kajvdh <Kaj@telenet.be>

--- src/BomberButti/BomberClient.java
+++ src/BomberButti/BomberClient.java
@@ -1,13 +1,72 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package BomberButti;
-
+import java.io.*; 
+import java.net.*;
 /**
- *
+ * BomberClient
+ * Hier wordt de verbinding opgezet met de Server (als die beschikbaar is)
  * @author Kaj
  */
 public class BomberClient {
-    
+    public static void main(String args[]) throws Exception {
+        try {
+            String serverHostname = new String ("127.0.0.1");
+
+            if (args.length > 0)
+                serverHostname = args[0];
+  
+            BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in)); 
+  
+            DatagramSocket clientSocket = new DatagramSocket(); 
+
+            InetAddress IPAddress = InetAddress.getByName(serverHostname); 
+            System.out.println ("Attemping to connect to " + IPAddress + 
+                                ") via UDP port 9876");
+
+            byte[] sendData = new byte[1024]; 
+            byte[] receiveData = new byte[1024]; 
+
+            System.out.print("Enter Message: ");
+            String sentence = inFromUser.readLine(); 
+            sendData = sentence.getBytes();         
+
+            System.out.println ("Sending data to " + sendData.length + 
+                                " bytes to server.");
+            DatagramPacket sendPacket = 
+               new DatagramPacket(sendData, sendData.length, IPAddress, 9876); 
+
+            clientSocket.send(sendPacket); 
+
+            DatagramPacket receivePacket = 
+               new DatagramPacket(receiveData, receiveData.length); 
+
+            System.out.println ("Waiting for return packet");
+            clientSocket.setSoTimeout(10000);
+
+            try {
+                clientSocket.receive(receivePacket); 
+                String modifiedSentence = 
+                    new String(receivePacket.getData()); 
+
+                InetAddress returnIPAddress = receivePacket.getAddress();
+
+                int port = receivePacket.getPort();
+
+                System.out.println ("From server at: " + returnIPAddress + 
+                                    ":" + port);
+                System.out.println("Message: " + modifiedSentence); 
+
+            }
+            catch (SocketTimeoutException ste) {
+                System.out.println ("Timeout Occurred: Packet assumed lost");
+            }
+  
+            clientSocket.close(); 
+        }
+        catch (UnknownHostException ex) { 
+            System.err.println(ex);
+        }
+        catch (IOException ex) {
+            System.err.println(ex);
+        }
+    }
 }
--- src/BomberButti/BomberServer.java
+++ src/BomberButti/BomberServer.java
@@ -1,13 +1,54 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package BomberButti;
 
+import java.io.*; 
+import java.net.*; 
 /**
- *
+ * BomberServer
+ * Hier wordt de Server opgezet waarop Clients kunnen connecteren om vervolgens tegen elkaar te spelen
  * @author Kaj
  */
 public class BomberServer {
-    
+    public static void main(String args[]) throws Exception  {
+        try {
+            DatagramSocket serverSocket = new DatagramSocket(9876); //Socket openen
+  
+            byte[] receiveData = new byte[1024]; //Ontvangen gegevens
+            byte[] sendData  = new byte[1024];  //Te verzenden gegevens
+
+            while(true) {
+                receiveData = new byte[1024]; 
+
+                //Ontvangen data (als die er is) ophalen
+                DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length); 
+
+                System.out.println ("Waiting for datagram packet");
+
+                serverSocket.receive(receivePacket); 
+
+                String sentence = new String(receivePacket.getData()); 
+
+                InetAddress IPAddress = receivePacket.getAddress(); 
+
+                int port = receivePacket.getPort(); 
+
+                System.out.println ("From: " + IPAddress + ":" + port);
+                System.out.println ("Message: " + sentence);
+
+                String capitalizedSentence = sentence.toUpperCase(); 
+
+                sendData = capitalizedSentence.getBytes(); 
+
+                DatagramPacket sendPacket = 
+                   new DatagramPacket(sendData, sendData.length, IPAddress, 
+                                     port); 
+
+                serverSocket.send(sendPacket); 
+
+              } 
+        }
+        catch(SocketException ex) {
+            System.out.println("UDP Port 9876 is occupied.");
+            System.exit(1);
+        }
+    }
 }
